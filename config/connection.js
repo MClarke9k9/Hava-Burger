3810c766960f12922eb1b09f001017ff6c315a99
Mark Clarke <the9k9@yahoo.com>
2020-10-11 22:53:07 -0400

    app pushed

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql"); {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8181,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+})
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name,isDevoured) VALUES ("Bacon Burger",0);
+INSERT INTO burgers (name,isDevoured) VALUES ("3 Cheese Burger",0);
+INSERT INTO burgers (name,isDevoured) VALUES ("McBang Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Black Bean Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Double Cheeseburger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Chili Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Turkey Burger",1);
