64040dc324bb12be6ba9077075b35f5280dde7fc
Mark Clarke <the9k9@yahoo.com>
2020-10-12 21:51:19 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,9 @@
-const mysql = require("mysql"); {
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = mysql.createConnection({
         host: "localhost",
         port: 8181,
@@ -6,7 +11,10 @@ const mysql = require("mysql"); {
         password: "root",
         database: "burgers_db"
     });
-}
+};
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
